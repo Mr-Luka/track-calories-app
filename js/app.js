a7733b0176f4fc9d06c4105f1d263ea5ea8749cc
Luka <luka.luka.luka93@gmail.com>
2024-09-25 13:06:25 -0700

    display calories limit and total

--- js/app.js
+++ js/app.js
@@ -4,16 +4,40 @@ class CalorieTracker {
         this._totalCalories = 0;
         this._meals = [];
         this._workouts = [];
+
+        this._displayCaloriesLimit();
+        this._displayCaloriesTotal();
     }
+
+    // Public Methods / API
 
     addMeal(meal) {
         this._meals.push(meal);
         this._totalCalories += meal.calories;
+        this._render();
     }
 
     addWorkout(workout){
         this._workouts.push(workout);
         this._totalCalories -= workout.calories;
+        this._render();
+    }
+
+    // Private Methods
+
+    _displayCaloriesTotal () {
+        const totalCaloriesEl = document.querySelector('#calories-total');
+        totalCaloriesEl.innerHTML = this._totalCalories;
+    }
+
+    _displayCaloriesLimit () {
+        const calorieLimitEl = document.querySelector('#calories-limit');
+        calorieLimitEl.innerHTML = this._calorieLimit;
+    }
+
+
+    _render(){
+        this._displayCaloriesTotal();
     }
 }
 
@@ -37,7 +61,9 @@ class Workout {
 const tracker = new CalorieTracker();
 
 const breakfast = new Meal('Breakfast', 400);
+const lunch = new Meal('Lunch', 350);
 tracker.addMeal(breakfast);
+tracker.addMeal(lunch);
 
 const run = new Workout('Morning Run', 300);
 tracker.addWorkout(run);
